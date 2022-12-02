e0229cbb7710d040626db75efe4aab779500b794
Steve Gardiner <stevegardiner26@aol.com>
2022-12-01 21:52:00 -0500

    Restructure Inputs to a Folder

--- day1.rb
+++ day1.rb
@@ -1,5 +1,5 @@
 # Part 1
-puts File.read('day1_input.txt').split(/\n\n/).map!{|elf| elf.split(/\n/).map(&:to_i).sum}.max
+puts File.read('inputs/day1.txt').split(/\n\n/).map!{|elf| elf.split(/\n/).map(&:to_i).sum}.max
 
 # Part 2
-puts File.read('day1_input.txt').split(/\n\n/).map!{|elf| elf.split(/\n/).map(&:to_i).sum}.max(3).sum
+puts File.read('inputs/day1.txt').split(/\n\n/).map!{|elf| elf.split(/\n/).map(&:to_i).sum}.max(3).sum
