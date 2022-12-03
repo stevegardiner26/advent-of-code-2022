81b6052f497654dc9e7c602b96ddd1e9a5da26da
Steeeve <stevegardiner26@aol.com>
2022-12-03 16:47:48 -0500

    Revised Part 2

--- day2.rb
+++ day2.rb
@@ -17,36 +17,13 @@ puts score
 input = File.read('inputs/day2.txt')
 rounds = input.split(/\n/)
 score = 0
-
+keys = {A: 1, B: 2, C: 3, X: 1, Y: 2, Z: 3}
+strats = [[3, 1, 2], [1, 2, 3], [2, 3, 1]]
 rounds.each do |round|
-  opponent, you = round.split(" ")
-  if you == "X" # HAVE TO LOOSE
-    if opponent == "A"
-      score += 3
-    elsif opponent == "B"
-      score += 1
-    elsif opponent == "C"
-      score += 2
-    end
-  elsif you == "Y" # HAVE TO DRAW
-    if opponent == "A"
-      score += 1
-    elsif opponent == "B"
-      score += 2
-    elsif opponent == "C"
-      score += 3
-    end
-    score += 3
-  elsif you == "Z" # HAVE TO WIN
-    if opponent == "B"
-      score += 3
-    elsif opponent == "C"
-      score += 1
-    elsif opponent == "A"
-      score += 2
-    end
-    score += 6
-  end
+  opponent, you = round.split(" ").map{|i| keys[i.to_sym]}
+  score += strats[you - 1][opponent-1]
+  score += 6 if you == 3
+  score += 3 if you == 2
 end
 
 puts score
